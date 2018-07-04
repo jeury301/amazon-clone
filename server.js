29acbcc234ee04b25beb06babe18b1e82fafd128
Jeury Mejia <jeurymejia@nypl.org>
2018-07-04 17:51:29 -0400

    Create and connect mongolab database to application

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // loading packages
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 // creating express application
 var app = express();
+
+const options = {
+  useNewUrlParser: true
+}
+
+// connecting to mongolab DB
+mongoose.connect('mongodb://amazon-clone:amazon-clone123@ds127851.mlab.com:27851/amazon-clone-db',options,function(err){
+  if(err){
+    console.log("Connection error: "+err);
+  } else{
+    console.log("Connected to the database");
+  }
+})
 
 // middleware - logging changes on server
 app.use(morgan('dev'))

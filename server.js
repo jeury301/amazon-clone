2828bab502325d09c5738aaeb1ab8e0cb51bd1a8
Jeury Mejia <jeurymejia@nypl.org>
2018-07-04 16:49:21 -0400

    Add and activate morgan library to capture http logging on console

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
-
+var morgan = require('morgan');
 var app = express();
 
+// middleware
+// logging changes on server
+app.use(morgan('dev'))
+
+// application entry point
+app.get('/', function(req, res){
+  var name = "Jeury";
+  res.json("My name is "+name);
+});
+
+// listening on port 3000
 app.listen(3000, function(err){
   if(err) throw err;
   console.log("Server is runnning on port 3000");
-})
+});

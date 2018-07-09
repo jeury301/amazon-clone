1d87eac44be78b16123add78fb5e0c19c358b392
Jeury Mejia <jeury.mejia@gmail.com>
2018-07-08 21:53:47 -0400

    Create products page. Displaying products based on categories

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+var Product = require('../models/product')
 
 // application entry point
 router.get('/', function(req, res){
@@ -8,6 +9,19 @@ router.get('/', function(req, res){
 // about page
 router.get('/about', function(req, res){
   res.render('main/about');
+});
+
+// products for each category
+router.get('/products/:id', function(req, res, next){
+  Product
+    .find({category: req.params.id})
+    .populate('category')
+    .exec(function(err, products){
+      if(err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
 });
 
 module.exports = router;
